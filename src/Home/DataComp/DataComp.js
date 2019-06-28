5a96bde3aea9ce86c88b9af578319780307b5973
ѕσσяуα gαηgαяαנ <sooryagangaraj@gmail.com>
2019-06-28 23:26:22 +0530

    slider new UI

--- src/Home/DataComp/DataComp.js
+++ src/Home/DataComp/DataComp.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 class DataComp extends Component {
   constructor(props) {
     super(props);
-    this.state = { principal: "", months: "" };
+    this.state = { principal: "0", months: "0" };
   }
   handlePrincipalChange(event) {
     var value = event.target.value;
@@ -27,37 +27,68 @@ class DataComp extends Component {
       <div>
         <div className="card">
           <div className="card-body text-left">
-            <h2 className="card-title">
-              <b>Data Input</b>
-            </h2>
             <b className="card-text">
-              <form>
-                <input
-                  type="text"
-                  className={
-                    this.state.principal <= 5000 && this.state.principal >= 500
-                      ? "form-control valid-input"
-                      : "form-control invalid-input"
-                  }
-                  id="principalINP"
-                  placeholder="Loan Amount ($500 - $5000)"
-                  value={this.state.principal}
-                  onChange={this.handlePrincipalChange.bind(this)}
-                />
+              <div className="container">
+                <h4>Loan Amount ($500 - $5000)</h4>
+                <div className="row">
+                  <div className="col-sm">
+                    <input
+                      type="text"
+                      className={
+                        this.state.principal <= 5000 &&
+                        this.state.principal >= 500
+                          ? "form-control valid-input"
+                          : "form-control invalid-input"
+                      }
+                      id="principalINP"
+                      placeholder="amount"
+                      value={this.state.principal}
+                      onChange={this.handlePrincipalChange.bind(this)}
+                    />
+                  </div>
+                  <div className="col-sm">
+                    <div class="slidecontainer">
+                      <input
+                        type="range"
+                        min="500"
+                        max="5000"
+                        onChange={this.handlePrincipalChange.bind(this)}
+                        className="slider"
+                        id="prinRange"
+                      />
+                    </div>
+                  </div>
+                </div>
                 <br />
-                <input
-                  type="text"
-                  className={
-                    this.state.months <= 24 && this.state.months >= 6
-                      ? "form-control valid-input"
-                      : "form-control invalid-input"
-                  }
-                  id="MonthsINP"
-                  placeholder="Loan Duration ( 6 - 24 months)"
-                  value={this.state.months}
-                  onChange={this.handleMonthsChange.bind(this)}
-                />
-              </form>
+                <h4>Loan Duration ( 6 - 24 months)</h4>
+                {/* months */}
+                <div className="row">
+                  <div className="col-sm">
+                    <input
+                      type="text"
+                      className={
+                        this.state.months <= 24 && this.state.months >= 6
+                          ? "form-control valid-input"
+                          : "form-control invalid-input"
+                      }
+                      id="MonthsINP"
+                      placeholder="months"
+                      value={this.state.months}
+                      onChange={this.handleMonthsChange.bind(this)}
+                    />
+                  </div>
+                  <div className="col-sm">
+                    <input
+                      type="range"
+                      min="6"
+                      max="24"
+                      onChange={this.handleMonthsChange.bind(this)}
+                      className="slider"
+                      id="montRange"
+                    />
+                  </div>
+                </div>
+              </div>
             </b>
           </div>
         </div>
