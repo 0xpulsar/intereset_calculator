352bd222b1c144d6838dbf69acfdb420f63a4e54
ѕσσяуα gαηgαяαנ <sooryagangaraj@gmail.com>
2019-06-28 13:12:08 +0530

    Updates

---
+++ precache-manifest.a5799278811014a1840e8b8ab6f05306.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e271bfb79d76a80842d61ae5e4c287cd",
+    "url": "/intereset_calculator/index.html"
+  },
+  {
+    "revision": "bae07c4e229f89d16860",
+    "url": "/intereset_calculator/static/css/main.8a6c682c.chunk.css"
+  },
+  {
+    "revision": "74661345f94ea5523c0e",
+    "url": "/intereset_calculator/static/js/2.ef28a6bb.chunk.js"
+  },
+  {
+    "revision": "bae07c4e229f89d16860",
+    "url": "/intereset_calculator/static/js/main.f7c1b7b7.chunk.js"
+  },
+  {
+    "revision": "39d40ed92375a917fb36",
+    "url": "/intereset_calculator/static/js/runtime~main.43a589d5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/intereset_calculator/static/media/logo.5d5d9eef.svg"
+  }
+]);
