b92412c59a86a129ea619301d5e8f7824aaf8d1f
ѕσσяуα gαηgαяαנ <sooryagangaraj@gmail.com>
2019-06-28 23:56:01 +0530

    Update src/nav/nav.js

--- src/nav/nav.js
+++ src/nav/nav.js
@@ -7,17 +7,23 @@ class Nav extends Component {
   }
   render() {
     return (
-      <nav className="navbar navbar-light bg-light">
-        <a className="navbar-brand" href="#">
-          <img
-            src="https://fullthrottlelabs.com/img/logo/logo-2.png"
-            width="80"
-            height="30"
-            className="d-inline-block align-top"
-            alt=""
-          />
-          <b>FullThrottle Labs</b>
-        </a>
+      <nav className="navbar navbar-light bg-light justify-content-between">
+        <h4>
+          <a className="navbar-brand" href="#">
+            <img
+              src="https://fullthrottlelabs.com/img/logo/logo-2.png"
+              width="80"
+              height="30"
+              className="d-inline-block align-top"
+              alt=""
+            />
+
+            <b>FullThrottle Labs</b>
+          </a>
+        </h4>
+        <h4>
+          <b>Loan Interest Calculator</b>
+        </h4>
       </nav>
     );
   }
